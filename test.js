c1fafa79236debee01362f222ec0d2b9187bff25
hung <hunglt.ee@gmail.com>
2023-08-05 10:59:54 +0700

    learn sortable

---
+++ test.js
@@ -0,0 +1,19 @@
+const trElements = document.getElementById('sortable-list');
+$(document).ready(function() {
+  const sortable = new Sortable(document.getElementById('sortable-list'), {
+    handle: '.handle',
+    animation: 150,
+    ghostClass: 'ghost',
+    dragClass: 'bg-gradient-primary', //drag
+    forceFallback: true,
+    onStart: function(evt) {
+      
+    },
+    onEnd: function(evt) {
+      
+    },
+
+  });
+});
+
+// https://github.com/SortableJS/Sortable/issues/1268
